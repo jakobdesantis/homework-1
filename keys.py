3fd9fdd938523efb737022e4d6dba59d6d949733
jakobdesantis <115081169+jakobdesantis@users.noreply.github.com>
2022-12-02 13:10:18 +0100

    Keys hinzugefügt
    Keys für die Twitter-Aufgabe

---
+++ keys.py
@@ -0,0 +1,19 @@
+# PYTHON FILE WITH TWITTER KEYS
+
+"""
+This file contains your Twitter keys
+
+    Replace the text with your keys
+    Keep the quotation marks
+
+"""
+
+consumer_key='aZoBvmcQrSV9NJp6uSZdDL087'
+
+consumer_secret='0TWviDLzulKS0LzAOUrNi1OiS7py09Msg2sS1Sardwd3ROZB8T'
+
+access_token='1582676615582081027-8BILiG88TtN7NI1dNgLXMYwkfQ9EfE'
+
+access_token_secret='ri2XvQYOzbxuVWPqbbBebRQJMM5Bw8NqK4p8md8d3IueJ'
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAIJgiQEAAAAA78S8U3akd18bRTjWNGt7nXu98qk%3D8mma2cSoN2Asjnn9nYOP0cD1RJTuFNYyKLasnL33xB3OZ2qn40'
