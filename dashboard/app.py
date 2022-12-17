d360af477d4d9b4affcd1cb193fb55eb2b355106
MelinaFuchs <mf183@hdm-stuttgart.de>
2022-12-17 11:10:22 +0100

    Bearbeitung Aufgabe 5

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,8 +7,8 @@ import pandas as pd
 import altair as alt
 
 from pathlib import Path
-#import datetime as dt
-#from datetime import datetime
+import datetime as dt
+from datetime import datetime
 
 #-------------------
 # DATA
@@ -18,8 +18,9 @@ home_path = str(Path.home())
 
 # Data import
 #df = pd.read_csv('C:\Users\User\Documents\Studium\Medienmanagement\HdM Stuttgart\Semester 4\BigData\data.csv')
-#df = pd.read_csv('../data/external/data2.csv')
-df = pd.read_csv('C:/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
+df = pd.read_csv('../data/external/data2.csv')
+#df = pd.read_csv('C:/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
+#df = pd.read_csv('C:\Users\User\Downloads\homework-1-main\homework-1-main\data\external\data.csv')
 #df = pd.read_csv('https://github.com/jakobdesantis/homework-1/blob/main/data/external/data.csv')
 #df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv?token=GHSAT0AAAAAABZZVTDROHOMKD4MZSBLNI2QY445DAQ')
 
