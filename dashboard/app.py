e3010b6e4871401ca0af9032c7d712e0c8cd54ca
MelinaFuchs <mf183@hdm-stuttgart.de>
2022-12-17 09:34:57 +0100

    Pfad bearbeiten Aufgabe 5

--- dashboard/app.py
+++ dashboard/app.py
@@ -14,10 +14,12 @@ from datetime import datetime
 # DATA
 
 # Data import
-#df = pd.read_csv('/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
-#df = pd.read_csv('../data/external/data.csv')
+#df = pd.read_csv('C:\Users\User\Documents\Studium\Medienmanagement\HdM Stuttgart\Semester 4\BigData\data.csv')
+#df = pd.read_csv('../data/external/data2.csv')
+df = pd.read_csv('C:/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
 #df = pd.read_csv('https://github.com/jakobdesantis/homework-1/blob/main/data/external/data.csv')
-df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv?token=GHSAT0AAAAAABZZVTDROHOMKD4MZSBLNI2QY445DAQ')
+#df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv?token=GHSAT0AAAAAABZZVTDROHOMKD4MZSBLNI2QY445DAQ')
+
 
 # Data transformation
 #-----------
