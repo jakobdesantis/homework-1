b9c8640d70b5ae3ca6105d18d6693794362c00ab
MelinaFuchs <mf183@hdm-stuttgart.de>
2022-12-17 15:03:26 +0100

    Bearbeitung Aufgabe 5

--- dashboard/app.py
+++ dashboard/app.py
@@ -18,9 +18,9 @@ home_path = str(Path.home())
 
 # Data import
 #df = pd.read_csv('C:\Users\User\Documents\Studium\Medienmanagement\HdM Stuttgart\Semester 4\BigData\data.csv')
-df = pd.read_csv('../data/external/data2.csv')
+#df = pd.read_csv('../data/external/data2.csv')
 #df = pd.read_csv('C:/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
-#df = pd.read_csv('C:\Users\User\Downloads\homework-1-main\homework-1-main\data\external\data.csv')
+df = pd.read_csv('C:\Users\User\Downloads\homework-1-main\homework-1-main\data\external\data.csv')
 #df = pd.read_csv('https://github.com/jakobdesantis/homework-1/blob/main/data/external/data.csv')
 #df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv?token=GHSAT0AAAAAABZZVTDROHOMKD4MZSBLNI2QY445DAQ')
 
@@ -88,4 +88,4 @@ c = chart_region
 # Show plot
 st.altair_chart(c, use_container_width=True)
 
-#restliche Graphen einfügen (in anderer Datei vorbeireitet)
+#restliche Graphen einfügen (in anderer Datei vorbereitet)
