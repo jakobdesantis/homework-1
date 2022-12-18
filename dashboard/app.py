eaa0892f4ba19b21910b72e3d63f3104c0c4231c
MatthiasMeinke <92295654+MatthiasMeinke@users.noreply.github.com>
2022-12-18 10:58:48 +0100

    Drei CSV-Dateien eingebunden

--- dashboard/app.py
+++ dashboard/app.py
@@ -17,15 +17,14 @@ from datetime import datetime
 #df = pd.read_csv('C:\Users\User\Documents\Studium\Medienmanagement\HdM Stuttgart\Semester 4\BigData\data.csv')
 #df = pd.read_csv('../data/external/data2.csv')
 #df = pd.read_csv('C:/Users/User/Documents/GitHub/homework-1/data/external/data.csv')
-df = pd.read_csv('C:\Users\User\Downloads\homework-1-main\homework-1-main\data\external\data.csv')
-#df = pd.read_csv('https://github.com/jakobdesantis/homework-1/blob/main/data/external/data.csv')
-#df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv?token=GHSAT0AAAAAABZZVTDROHOMKD4MZSBLNI2QY445DAQ')
+#df = pd.read_csv('C:\Users\User\Downloads\homework-1-main\homework-1-main\data\external\data.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data.csv')
+#df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data2.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/jakobdesantis/homework-1/main/data/external/data3.csv')
 
 
 # Data transformation
 df['Date'] = pd.to_datetime(df['date']).dt.strftime('%Y-%m-%d')
-#-----------
-df2 = df.drop(labels=range(5000, 250000), axis=0)
 #-----------
 df2["length"] = df2["content"].str.len()
 #----------
